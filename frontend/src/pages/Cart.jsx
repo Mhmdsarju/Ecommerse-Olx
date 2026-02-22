5bbf58eddac4237bdeb58790933fda5fca1fa655
Mhmdsarju <mhmdsarju@gmail.com>
2026-02-23 00:13:34 +0530

    new thing

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -61,7 +61,7 @@ const Cart = () => {
                 onClick={() => dispatch(removeFromCart(item._id))}
                 className="bg-red-500 text-white px-4 py-2 rounded"
               >
-                Remove
+                Remov
               </button>
             </div>
           ))
